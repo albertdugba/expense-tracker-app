bac9ef18815a711b47e1e4c0062fa79a11f2461c
Albert Dugba <albert.dugba@gmail.com>
2020-02-28 10:22:12 +0000

    switched from class compo to functional compo

--- src/App.js
+++ src/App.js
@@ -1,95 +1,24 @@
-import React, { Component } from "react";
-
-import uuid from "uuid";
-import "bootstrap/dist/css/bootstrap.css";
-
-import "./App.css";
-import FormInput from "./components/FormInput";
-import FormItem from "./components/FormItem";
+import React from "react";
 import Header from "./components/Header";
 
-class App extends Component {
-  state = {
-    expensesItem: [
-      {
-        id: uuid(),
-        date: "",
-        category: "",
-        description: "",
-        amount: 0,
-        editExpense: false
-      }
-    ],
-    total: 0
-  };
+import "./App.css";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
-  componentDidMount = () => {
-    // console.log("updated", this.state);
-    const amounts = this.state.expensesItem.map(item => Number(item.amount));
-    const total = amounts.reduce((prev, next) => prev + next);
-    this.setState({ total });
-    // return this.setState({ total });
-  };
-
-  // addFormInput = ({ date, category, description, amount, total }) => {
-  //   console.log(date);
-  //   const newExpenses = {
-  //     id: uuid(),
-  //     date,
-  //     category,
-  //     description,
-  //     amount,
-  //     total: sum
-  //   };
-  //   console.log(newExpenses);
-  //   return this.setState({
-  //     expensesItem: [...this.state.expensesItem, newExpenses]
-  //   });
-  // };
-
-  handleSubmit = (event, data) => {
-    event.preventDefault();
-    const newState = [...this.state.expensesItem, data];
-    const amounts = newState.map(item => Number(item.amount));
-    const total = amounts.reduce((prev, next) => prev + next);
-    this.setState({ expensesItem: newState, total });
-    // console.log(newState);
-  };
-
-  handleDelete = (e, id) => {
-    // console.log(id);
-    const expensesItem = this.state.expensesItem.filter((item, i) => i !== id);
-    this.setState({ expensesItem });
-  };
-
-  handleEdit = id => {
-    const items = this.state.expensesItem.filter(item => item.id === id);
-    console.log(items);
-    this.setState({
-      editExpense: true,
-      expensesItem: items
-    });
-  };
-  render() {
-    // console.log(this.state);
-    const { id } = this.state.expensesItem;
-    return (
-      <div>
-        <Header />
-        <FormInput
-          // addFormInput={this.addFormInput}
-          submit={this.handleSubmit}
-        />
-        <FormItem
-          key={id}
-          expenses={this.state.expensesItem}
-          handleDelete={this.handleDelete}
-          handleEdit={this.handleEdit}
-          total={this.state.total}
-        />
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,7 @@ import { FaRegMoneyBillAlt } from "react-icons/fa";
 import "../App.css";
 
 const Header = () => {
-  return (
-    <div className="nav-container">
-      <div className="container navBar">
-        <FaRegMoneyBillAlt className="logo" />
-        <nav className="ul">
-          <ul>
-            <li> Home</li>
-            <li>Add</li>
-            <li />
-            <li>Total</li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+  return <h2>Expense Tracker</h2>;
 };
 
 export default Header;
